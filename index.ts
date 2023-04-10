88155043d3a045ac342f0e690ac499d70e83770e
Nathael Bonnal <kirua48000@gmail.com>
2023-04-11 00:25:25 +0200

    :construction: export file

---
+++ index.ts
@@ -0,0 +1,11 @@
+/*
+ * @reforged/lucid
+ *
+ * (c) Bonnal Nathaël <pro.nathaelbonnal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export { Logic } from './src/logic'
+export { Tree } from './src/tree'
