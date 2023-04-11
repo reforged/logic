62f31a92df92b2438b0ae3ebc61edf8a8cda42fb
Nathael Bonnal <kirua48000@gmail.com>
2023-04-11 18:29:29 +0200

    :construction: Class creation for Input props

---
+++ src/token/token-input.ts
@@ -0,0 +1,51 @@
+import {Logic} from "../logic";
+import {Token} from "./token";
+import {ConditionContract, RowCondition} from "@ioc:Reforged/Logic";
+import {Prop, SymbContract} from "@ioc:Reforged/Formule";
+
+export class TokenInput<T> extends Token<T>{
+  constructor(prop: Prop, data: T) {
+    super(prop, data)
+    if (prop.kind !== 'symb' || prop.data.type !== 'input') {
+      console.log("ERROR")
+    }
+  }
+
+  public contains (): boolean {
+    const data = this.getData()
+    const prop = this.getProp() as SymbContract
+    const value: string = data[prop.data.field as keyof T]
+    return value.toLowerCase().includes(prop.data.value.toLowerCase())
+  }
+
+  public notContains (): boolean {
+    return !this.contains()
+  }
+
+  public is (): boolean {
+    const data = this.getData()
+    const prop = this.getProp() as SymbContract
+    const value: string = data[prop.data.field as keyof T]
+
+    return value.toLowerCase() === prop.data.value.toLowerCase()
+  }
+
+  public isNot (): boolean {
+    return !this.is()
+  }
+
+  public evaluate(): boolean {
+    const prop = this.getProp() as SymbContract
+
+    switch (prop.data.operator) {
+      case "contains":
+        return this.contains()
+      case "not contains":
+        return this.notContains()
+      case "is not":
+        return this.isNot()
+      case "is":
+        return this.is()
+    }
+  }
+}
